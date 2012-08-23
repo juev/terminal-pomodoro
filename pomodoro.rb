aa6dd526c219ac466004eee4a13a4a7c84ce9d77
Evsyukov Denis <denis@evsyukov.org>
2012-08-23 22:01:33 +0400

    Change algoritm (adding function) and delete .bundle from git

--- pomodoro.rb
+++ pomodoro.rb
@@ -7,26 +7,31 @@ require "terminal-notifier"
 
 WORK = 25 * 60 # 25 min in sec
 REST = 5 * 60 # 5 min in sec
-LONG = 10 * 60 # adding 10 min Rest in sec
+LONG = 15 * 60 # adding 15 min Rest in sec
+REPEAT = 4 # count for long rest
+
+def period(t, message, color)
+  TerminalNotifier.notify(message, :title => 'Pomodoro', :group => 'Pomodoro')
+  puts "#{message} #{t / 60} min.".colorize( color )
+  sleep t # sec  
+end
+
+def exit?
+  TerminalNotifier.notify('New workout?', :title => 'Pomodoro', :group => 'Pomodoro')
+  puts "If you want to continue, press Return, or Ctrl+C to exit".colorize( :yellow )
+  begin
+    gets
+  rescue Exception => e
+    puts "\nThank you for using Pomodoro!\nGoodbye!".colorize( :red )
+    exit
+  end  
+end
 
 while true
-  (1..4).each{
-    TerminalNotifier.notify('Go!!!', :title => 'Pomodoro', :subtitle => 'Begining', :group => 'Pomodoro')
-    puts "Work #{WORK} sec.".colorize( :blue )
-    sleep WORK # sec
-    TerminalNotifier.notify('Its time to relax!', :title => 'Pomodoro', :subtitle => 'Well done', :group => 'Pomodoro')
-    puts "Rest #{REST} sec.".colorize( :gray )
-    sleep REST #sec
-    TerminalNotifier.notify('Let us work?', :title => 'Pomodoro', :subtitle => 'Quest', :group => 'Pomodoro')
-    puts "If you want to continue, press Return, or Ctrl+C to exit".colorize( :yellow )
-    begin
-      gets
-    rescue Exception => e
-      puts "\nThank you for using Pomodoro!\nGoodbye!"
-      exit
-    end
+  (0..REPEAT).each{ |count|
+    period(WORK, "Work", :blue)
+    period(REST, "Rest", :gray) if count < REPEAT
+    begin period(LONG, "Long Rest", :red); count = 0; end if count == REPEAT
+    exit?
   }
-  puts "Rest #{LONG} sec. additional...".colorize( :red )
-  TerminalNotifier.notify('Long Rest!', :title => 'Pomodoro', :subtitle => 'Rest', :group => 'Pomodoro')  
-  sleep LONG #sec  
 end
