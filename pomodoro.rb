310819dcceeaadfa13326a2f962adfaac213e29b
Evsyukov Denis <denis@evsyukov.org>
2012-08-23 22:13:36 +0400

    Reformat oneline if

--- pomodoro.rb
+++ pomodoro.rb
@@ -5,10 +5,10 @@ require "bundler/setup"
 require "colorize"
 require "terminal-notifier"
 
-WORK = 25 * 60 # 25 min in sec
-REST = 5 * 60 # 5 min in sec
-LONG = 15 * 60 # adding 15 min Rest in sec
-REPEAT = 4 # count for long rest
+WORK = 1#25 * 60 # 25 min in sec
+REST = 1#5 * 60 # 5 min in sec
+LONG = 1#15 * 60 # adding 15 min Rest in sec
+REPEAT = 2#4 # count for long rest
 
 def period(t, message, color)
   TerminalNotifier.notify(message, :title => 'Pomodoro', :group => 'Pomodoro')
@@ -30,7 +30,9 @@ end
 while true
   (0..REPEAT).each{ |count|
     period(WORK, "Work", :blue)
-    count == REPEAT ? period(LONG, "Long Rest", :red) : period(REST, "Rest", :gray)
+    count == REPEAT ? \
+      period(LONG, "Long Rest", :red) : \
+      period(REST, "Rest", :gray)
     # period(REST, "Rest", :gray) if count < REPEAT
     # period(LONG, "Long Rest", :red) if count == REPEAT
     exit?
